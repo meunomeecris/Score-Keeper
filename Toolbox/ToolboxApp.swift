612e8a19e855d0b7b3e8b09538c9761664b700b3
Cris Messias <meunomeecriss@gmail.com>
2025-02-18 14:07:24 +0100

    fix code, replace back views

--- Toolbox/ToolboxApp.swift
+++ Toolbox/ToolboxApp.swift
@@ -6,14 +6,12 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct SwiftTutorialsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: Friend.self)
         }
     }
 }
